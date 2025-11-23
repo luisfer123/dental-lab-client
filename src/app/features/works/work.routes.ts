12ca4901f96e8426f12bd6456e8bec68a0f2d021
luisfer123 <luisfermtnz@gmail.com>
2025-11-22 22:57:30 -0600

    Reorganized compoenets for Work. components move works -> works/components

--- src/app/features/works/work.routes.ts
+++ src/app/features/works/work.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from "@angular/router";
-import { WorkListComponent } from "./work-list/work-list.component";
-import { WorkDetailComponent } from "./work-detail/work-detail.component";
+import { WorkListComponent } from "./components/work-list/work-list.component";
+import { WorkDetailComponent } from "./components/work-detail/work-detail.component";
 
 export const Work_ROUTES: Routes = [
     {path: '', component: WorkListComponent},
